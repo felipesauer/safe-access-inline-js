ed2573490c2f61f7bf24cb7d236d5215cd479e78
felipesauer <felipesauer11@hotmail.com>
2026-03-12 22:49:20 -0400

    feat(js): add toToml() to TransformableInterface

--- src/contracts/transformable.interface.ts
+++ src/contracts/transformable.interface.ts
@@ -2,6 +2,7 @@ export interface TransformableInterface {
     toArray(): Record<string, unknown>;
     toJson(pretty?: boolean): string;
     toObject(): Record<string, unknown>;
+    toToml(): string;
     toYaml(): string;
     toXml(rootElement?: string): string;
     transform(format: string): string;
